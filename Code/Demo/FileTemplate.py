35aaa5fd3c86fddfdaf8fd5f3a44902f1390f346
AUnicyclingProgrammer <github@jonathan.theballards.us>
2024-05-28 14:27:51 -0600

    Create Demo Folder

---
+++ Code/Demo/FileTemplate.py
@@ -0,0 +1,14 @@
+# ----- Imports -----
+
+# ----- Global Values ----
+
+# ----- Global Classes -----
+
+# ----- Methods and Functions -----
+
+# ----- Utility Classes -----
+
+# ----- Begin Program -----
+if __name__ == "__main__":
+    print("Printed Output")
+# 
